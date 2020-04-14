eb5ef834480a8452ba15ae232fb3b56ab4893bfb
killjoe294@gmail.com <siLvercross02>
2020-04-14 17:14:01 +0800

    11 Commit PublicRoutes and PrivateRoutes

--- src/components/Widgets/NewsSlider/slider.js
+++ src/components/Widgets/NewsSlider/slider.js
@@ -14,17 +14,38 @@ class NewsSlider extends Component {
         firebaseArticles.limitToFirst(3).once('value')
         .then((snapshot) => {
             const news = firebaseLooper(snapshot);
-            news.forEach((item,i) =>{
+            // news.forEach((item,i) =>{
+            //     firebase.storage().ref('images')
+            //     .child(item.image).getDownloadURL()
+            //     .then(url => {
+            //         news[i].image = url;
+            //         this.setState({
+            //             news
+            //         })
+            //     })
+            // });
+
+            const asyncFunction = (item, i, cb) => {
                 firebase.storage().ref('images')
                 .child(item.image).getDownloadURL()
                 .then(url => {
                     news[i].image = url;
-                    this.setState({
-                        news
-                    })
+                    cb();
+                })
+            }
+
+            let requests = news.map((item,i) => {
+                return new Promise((resolve) => {
+                    asyncFunction(item, i, resolve)
                 })
             })
-          
+
+            Promise.all(requests).then(()=> {
+                this.setState({
+                    news  
+                })
+            })
+
         });
 
 
